4509d0a609d3fa50a008052a78998b3c0a58bd37
sevincesedova <161349938+sevincesedova@users.noreply.github.com>
2024-04-03 13:09:50 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// // task1
+// const products=[
+//     {
+//         id:1,
+//         name: "iphone 15",
+//         qiymet: 1000,
+        
+      
+        
+//     },
+//     {
+//         id:2,
+//         name: "iphone 14",
+//         qiymet: 800,
+        
+// },
+//   {
+//         id:3,
+//         name: "samsung a21",
+//         qiymet: 500,
+        
+//   },
+//   {
+//         id:4,
+//         name: "iphone 11 ",
+//         qiymet: 600,
+       
+// },           
+// ]
+// let products_sort=products.sort((a,b)=>a.qiymet-b.qiymet)
+// products_foreach=products.forEach((x)=>console.log( x.qiymet))
+
+
+// task2
+// let massiv=[]
+// for(i=0; i<5;i++){
+//     massiv.push(Math.floor(Math.random(i)*9)+1)
+// }
+// console.log(massiv)
+
+// let cem=0
+// for(x=0; x<massiv.length; x++){
+//     cem+=massiv[x]
+// }
+// console.log(`${cem}`)
